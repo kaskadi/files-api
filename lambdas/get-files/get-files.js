70eb5f80a125b2e7ad84cbed2a5275daca6adaf3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-31 18:32:35 +0200

    Updated comments for future refactoring

--- lambdas/get-files/get-files.js
+++ lambdas/get-files/get-files.js
@@ -1,7 +1,6 @@
 // ************
 // Note: this lambda shouldn't need much update to handle any type of get files.
 // Ideas:
-//    - do not default to 'imgs/products/' for our path
 //    - be able to support CDN/S3 selection between public and private one, would probably need a bit more check for access control
 // ************
 
--- lambdas/get-signed-url/get-signed-url.js
+++ lambdas/get-signed-url/get-signed-url.js
@@ -1,3 +1,9 @@
+// ************
+// Note: this lambda shouldn't need much update to handle any type of upload files.
+// Ideas:
+//    - be able to support CDN/S3 selection between public and private one, would probably need a bit more check for access control
+// ************
+
 const AWS = require('aws-sdk')
 const createSignedUrl = require('./helpers/create-signed-url.js')
 const processRes = require('./helpers/process-response.js')
