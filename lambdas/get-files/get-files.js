fd9a1587741218dad743ef6b1dc8da8456451b65
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-31 15:28:56 +0200

    Removed default path logic

--- lambdas/get-files/get-files.js
+++ lambdas/get-files/get-files.js
@@ -9,19 +9,26 @@ const AWS = require('aws-sdk')
 const getData = require('./helpers/get-data')
 
 module.exports.handler = async (event) => {
-  const params = {
-    path: event.queryStringParameters ? event.queryStringParameters.path : 'imgs/products/',
-    s3: new AWS.S3({
-      region: 'eu-central-1',
-      apiVersion: '2006-03-01'
-    }),
-    bucketName: process.env.S3_PUBLIC_BUCKET
-  }
   const baseResponse = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*'
     }
+  }
+  if (!event.queryStringParameters || !event.queryStringParameters.path) {
+    return {
+      ...baseResponse,
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Please provide a valid path in your query string parameters...' })
+    }
+  }
+  const params = {
+    path: event.queryStringParameters.path,
+    s3: new AWS.S3({
+      region: 'eu-central-1',
+      apiVersion: '2006-03-01'
+    }),
+    bucketName: process.env.S3_PUBLIC_BUCKET
   }
   return await getData(params, baseResponse)
 }
